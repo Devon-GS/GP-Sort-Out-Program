3a47cb1db3a72fd6c0c7cc33327638d9b53dca54
Devon <ds.pydev@gmail.com>
2025-04-08 10:16:16 +0200

    Change data file to csv and clean up

--- main.py
+++ main.py
@@ -7,55 +7,48 @@ from openpyxl.styles import Alignment, Border, Side
 
 root = Tk()
 root.title('Sasol De Bron - GP Analyzer ')
-root.geometry('350x225')
+root.geometry('230x150')
 
 def gp_analysis():
-    # Get Less and Greater than amounts
+    # Get Less than amounts
     less = int(less_than_input.get())
-    greater = int(greater_than_input.get())
-
     less_than_input.delete(0,END)
-    greater_than_input.delete(0,END)
 
-    # Read in excell and convert to list
-    check_option = c.get()
-    if check_option == 0:
-        file = 'Analysis Info/Analysis Info.xls'
-    else:
-        path = os.getcwd()
-        file = filedialog.askopenfilename(initialdir=path, title='Select A File', filetypes=(("Excel 97-2003", '*.xls'),('Excel file',"*.xlsx")))
+    # Select .csv file
+    path = os.getcwd()
+    file = filedialog.askopenfilename(initialdir=path, title='Select A File', filetypes=(("CSV Files", '*.csv'),))
     
-    read_info = pd.read_excel(file, header=1)
+    # Read in data from selected csv file
+    read_info = pd.read_csv(file, header=1).dropna()
     data = read_info.to_numpy().tolist()
 
-    # Remove nan and unwated data from data list
+    # Remove unwated data from data list
     simple_list = []
     for x in data:
-        if x[0] == float("nan") or x == '----------------------------':
-            pass
-        elif type(x[0]) == int:
-            simple_list.append(x)
+        barcode = x[0].strip()
+        name = x[1].strip()
+        amt = x[2].strip()
 
+        simple_list.append([barcode, name, amt, x[3], x[4], x[5], x[6], x[7], x[8], x[9]])
+
+    # Run through list and execute less than amount
     printing_list = []
     for x in simple_list:
-        info.config(text='Checking')
-        # print(f'Checking if {x[1]} GP {x[-1]} <=-5 or >=5')
-        if x[-1] <=-less or x[-1] >=greater:
+        if x[-2] <= less:
             printing_list.append(x)
 
     items = len(printing_list) + 2
 
+    # Create Wwork book   
     wb = Workbook()
     ws = wb.active
     ws.title = 'Stock GP List'
-    # print('Create Excel Workbook')
 
     # Set Column Width 
     ws.column_dimensions['A'].width = 14
     ws.column_dimensions['B'].width = 35
 
-    # Set row names
-    # print('Create Column Names')
+    # Set column names
     ws['C1'] = 'Pack'
     ws['E1'] = 'Desired %'
     ws['F1'] = 'Sugg Incl'
@@ -75,7 +68,7 @@ def gp_analysis():
     ws['I2'] = 'Sys Cost'
     ws['J2'] = 'Sys Cost'
 
-    # # loop through sheet totals
+    # Loop through sheet totals
     i = 3
     while i <= items:
         for stock in printing_list:
@@ -92,41 +85,34 @@ def gp_analysis():
             
             ws[f'A{i}'].number_format = '0'
             i += 1
+
     info.config(text='Analysis Complete')
     f_name = file_name_input.get()
     if f_name == '':
-        wb.save(f'GP Analysis Less than {less} and Greater Than {greater}.xlsx')
+        wb.save(f'GP Analysis Less than {less}.xlsx')
     else:
         wb.save(f'{f_name}.xlsx')
         file_name_input.delete(0,END)
 
-less_than = Label(root, text='Less Than (-)')
+# Less than widget
+less_than = Label(root, text='Less Than')
 less_than.grid(row=0, column=0, padx=(10,0), pady=(10,0))
 
-less_than_input = Entry(root, text='Less Than (-)')
+less_than_input = Entry(root, text='Less Than')
 less_than_input.grid(row=0, column=1, padx=(10,0), pady=(10,0))
 
-greater_than = Label(root, text='Greater Than')
-greater_than.grid(row=1, column=0, padx=(10,0), pady=(10,0))
-
-greater_than_input = Entry(root, text='Greater Than')
-greater_than_input.grid(row=1, column=1, padx=(10,0), pady=(10,0))
-
+# File name widget
 file_name = Label(root, text='File Name')
 file_name.grid(row=2, column=0, padx=(10,0), pady=(10,0))
 
 file_name_input = Entry(root, text='File Name')
 file_name_input.grid(row=2, column=1, padx=(10,0), pady=(10,0))
 
-c = IntVar()
-check = Checkbutton(root, text='I will Select File', variable=c)
-check.grid(row=3, column=0, padx=(10,0), pady=(10,0))
-
-cal = Button(root, text='Start Analysis', command=gp_analysis)
-cal.grid(row=4, column=0, padx=(10,0), pady=(10,0))
+start_analysis_btn = Button(root, text='Start Analysis', command=gp_analysis)
+start_analysis_btn.grid(row=4, column=0, columnspan=2, sticky=W+E, padx=(10,10), pady=(10,10))
 
 info = Label(root, text='')
-info.grid(row=5, column=0)
+info.grid(row=5, column=0, columnspan=2)
 
 # Run program
 root.mainloop()
